a67ff30380599d156c6aad25fef95c2da16dd015
Layee <Layee@atguigu.com>
2022-09-06 13:12:01 +0800

    second commit

--- src/test/java/com/atguigu/test/GitTest.java
+++ src/test/java/com/atguigu/test/GitTest.java
@@ -6,5 +6,6 @@ import org.testng.annotations.Test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git222!");
     }
 }
